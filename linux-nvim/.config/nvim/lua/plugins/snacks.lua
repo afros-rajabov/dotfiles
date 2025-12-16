17ffe216aaf7e8d7bf83e3a3c656d5f14765c4b2
Afros Rajabov <afrosrajabov1207@mail.ru>
2025-12-17 01:55:40 +0700

    Toggle zen mode dim

--- linux-nvim/.config/nvim/lua/plugins/snacks.lua
+++ linux-nvim/.config/nvim/lua/plugins/snacks.lua
@@ -8,8 +8,33 @@ return {
       end,
       desc = "Preview Definition",
     },
+    {
+      "<leader>zd",
+      function()
+        local noice = require("noice")
+        noice.disable()
+        local snacks = require("snacks")
+        snacks.config.zen.toggles.dim = not snacks.config.zen.toggles.dim
+        snacks.toggle.zen():toggle()
+        snacks.toggle.zen():toggle()
+        noice.enable()
+      end,
+      desc = "Toggle Zen Dim",
+    },
   },
   opts = {
+    zen = {
+      toggles = {
+        dim = true,
+      },
+      show = {
+        statusline = true, -- can only be shown when using the global statusline
+      },
+      win = {
+        minimal = false,
+        backdrop = { transparent = false },
+      },
+    },
     terminal = {
       win = {
         position = "float",
