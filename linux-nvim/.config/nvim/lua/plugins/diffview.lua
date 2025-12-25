7e008dd242f1c3eab76d1c1680349ac76541d76b
Afros Rajabov <a.radzhabov@estesis.tech>
2025-12-25 12:59:16 +0700

    Diffview

---
+++ linux-nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewToggleFiles",
+    "DiffviewFocusFiles",
+  }, -- Commands to open it
+  config = true, -- This will run require("diffview").setup()
+}
